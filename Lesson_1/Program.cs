27dd6b0e4388364f1881070fd8f2bf1875acaec4
kseniaarshinova7 <ksenia.arshinova7@gmail.com>
2023-03-12 21:01:52 +0700

    initial commit

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}!");
+        }
+    }
+}
+
+
